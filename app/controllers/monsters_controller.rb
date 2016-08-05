09a57a7d9a583532dbf0b0fa444eb9ea5feea058
Mark Nuiver <mark@vrijblikken.nl>
2016-08-05 15:24:17 +0200

    Create a Show Dinosaur Page

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,5 +1,11 @@
 class MonstersController < ApplicationController
+
   def index
     @monsters = Monster.all
   end
+
+  def show
+    @monster = Monster.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "monsters#index"
 
   get "monsters" => "monsters#index"
+  get "monsters/:id" => "monsters#show", as: :monster
 
 end
